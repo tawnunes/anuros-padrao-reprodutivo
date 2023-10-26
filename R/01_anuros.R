c81104f6e908563285c0fe2f99d4536b2400d593
Tawane Nunes <taw.ynunes@gmail.com>
2023-10-26 12:35:04 -0300

    Organizando script de análises geral e exlcuindo arquivos, simplificando as informações para manter o foco no objetivo principal do trabalho

--- R/01_anuros.R
+++ R/01_anuros.R
@@ -7,7 +7,7 @@
 # 
 # Discentes: Jonathan, Ruth Oliveira, Tawane Nunes
 # 
-# Comportamento Reprodutivo em anuros
+# Padrões Reprodutivos em Anuros: Uma Análise Latitude-dependente
 # 
 # Script 01: Reunindo e organizando as bases de dados
 
@@ -39,122 +39,27 @@ anuros <- anuros %>%
 globalspecies <- read.csv("data/raw/Species_Database_wb_datanam.csv")
 
 #mudando nome da coluna para corresponder com anuros
-
 globalspecies <- globalspecies %>%
     rename(species = binomial)
-
-## DADOS AMPHIBIAM WEB -------------------------------------------------------
-
-# Dados provenientes do site amphibian web https://amphibiaweb.org/api/ws.html
-# acessando os dados em xml
-url <- "https://amphibiaweb.org/amphib_dump.xml"
-response <- GET(url)
-
-xml_content <- content(response, "text", encoding = "UTF-8")
-xml <- read_xml(xml_content)
-
-# extraindo as informação de cada espécie
-species_elements <- xml %>%
-    xml_find_all(".//species")
-
-# Extrair os elementos filhos de cada 'species' como uma lista
-species_data <- lapply(species_elements, function(species) {
-    data <- species %>%
-        xml_children() %>%
-        xml_text()
-    names(data) <- xml_name(xml_children(species))
-    return(data)
-})
-
-# Converter a lista em um dataframe
-anfibios_df <- as.data.frame(do.call(rbind, species_data))
-
-# Criando uma coluna com o nome da espécie
-
-anfibios_df <- anfibios_df %>% 
-    mutate(species = paste(genus, specificepithet, sep = "")) %>% 
-    mutate(species = str_replace_all(species, "\n", " "),# Substituir '\n' por espaço
-           species = str_replace_all(species, "\\s+", " ")) %>% # Remover espaços em branco extras
-    mutate(species = str_trim(species))
-
 
 # JUNTANDO AS INFORMAÇÕES -------------------------------------------------------------------------------------
 
 anuros_completo <- anuros %>% 
-    # extraindo apenas o range total (km²) de ocorrência e idenficação se o range é menor que 25 km²
-    left_join(globalspecies[,c(2,6,8:9)], by = "species", multiple = "first", keep = F) %>% 
-    # extraindo mais informações informações do amphibian web
-    left_join(anfibios_df[,c(13,18:24,26:27)], by = "species", keep = F)
+    # extraindo apenas a abrangência geográfica de ocorrência (km²) de ocorrência e idenficação se o range é menor que 25 km²
+    left_join(globalspecies[,c(2,8:9)], by = "species", multiple = "first", keep = F)
 
 
 # organizando planilha
 anuros_completo <- anuros_completo %>% 
+    #colocando as referências em uma coluna só
     mutate(Nelietal_refs = paste(reference1, reference2, reference3, reference4, sep = ", ")) %>% 
-    rename(occ_countries = isocc,
-           amphibianweb_refs = refs) %>% 
-    select(c(1:9, 14:26)) 
-
-# tirando caracteres de código html
+    # selecionando apenas as variáveis de interesse
+    select(c(1,2,6,14,16))
+    
+# formatando a limpando as referências
 anuros_completo <- anuros_completo %>% 
-    mutate_at(vars(14:21), ~str_replace_all(., "<.*?>", ""))
-
-# EXTRAINDO MAIS INFORMAÇÕES ----------------------------------------------------------------------
-
-## HABITOS DE VIDA: DIURNO OU NOTURNO -------------------------------------------------------------
-# habitos de vida (noturno)
-anuros_habit_nocturnal <- anuros_completo %>% 
-    select(species, life_history) %>% 
-    filter(grepl("nocturnal", life_history, ignore.case = T))
-
-# habitos de vida (diurno)
-anuros_habit_diurnal <- anuros_completo %>% 
-    select(species, life_history) %>% 
-    filter(grepl("diurnal", life_history, ignore.case = T))
-
-# ambos os termos no texto
-anuros_habit_both <- anuros_habit_diurnal %>% 
-    filter(species %in% anuros_habit_nocturnal$species)
-
-# habitos de vida (diurno)
-anuros_habit_diurnal <- anuros_habit_diurnal %>% 
-    filter(!species %in% anuros_habit_both$species)
-
-# adicionando a informação no datframe completo
-anuros_completo <- anuros_completo %>% 
-    mutate(habit = ifelse(species %in% anuros_habit_nocturnal$species, "nocturnal",
-                          ifelse(species %in% anuros_habit_diurnal$species, "diurnal", NA)))
-
-## CUIDADO PARENTAL -------------------------------------------------------------------------------------
-
-anuros_parental <- anuros_completo %>% 
-    select(species, life_history) %>% 
-    filter(grepl("parental care", life_history, ignore.case = T))
-
-anuros_guard <- anuros_completo %>% 
-    select(species, life_history) %>% 
-    filter(grepl("guards the eggs", life_history, ignore.case = T))
-# adicionando a informação no datframe completo
-anuros_completo <- anuros_completo %>% 
-    mutate(parental_care = ifelse(species %in% anuros_parental$species | species %in% anuros_guard$species,
-                                  1,0)) # zero pode ser NA
-
-## TERRITORIALIDADE --------------------------------------------------------------------------------------
-
-# listando os que já são classificados por Neli et al. 2014 como territorialistas
-anuros_territ <- anuros_completo %>% 
-    filter(evidences_territoriality != "NA")
-
-# extraindo a informação dos dados do amphibian web
-anuros_territ_aw <- anuros_completo %>% 
-    select(species, life_history) %>% 
-    filter(grepl("territorial", life_history, ignore.case = T))
-
-# adicionando a informação no datframe completo
-anuros_completo <- anuros_completo %>% 
-    mutate(territorial = ifelse(species %in% anuros_territ$species | species %in% anuros_territ_aw$species, 1, 0))
-
-# exportando como xlsx
-write_xlsx(anuros_completo, "data/processed/dados_anuros_completo.xlsx")
+    mutate(Nelietal_refs = gsub("NA","", Nelietal_refs),
+           Nelietal_refs = gsub("<e1>","a", Nelietal_refs))
 
 # exportando como csv
 
