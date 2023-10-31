f1bc0fdd1a71ca3b7d7d5c8e5b2d66326378b13f
Tawane Nunes <taw.ynunes@gmail.com>
2023-10-31 10:15:41 -0300

    incluin interação nos modelos e corrigindo pacotes utilizados, algum erro esta acontecendo no vif do primeiro modelo, antes estava normal

--- R/04_analises_ranidae.R
+++ R/04_analises_ranidae.R
@@ -15,6 +15,7 @@
 library(tidyverse) # manipulação dos dados
 library(ggplot2) # visualização gráfica
 library(car) # para teste de colinearidade
+library(DHARMa) # validação do modelo por análise dos resíduos
 
 # IMPORTANTDO OS DADOS -----------------------------------------------------------------------------------------
 
@@ -34,22 +35,51 @@ ranidae_completo <- ranidae %>%
     mutate(f.breeding_pattern = as.factor(tolower(breeding_pattern)),
            prolonged = ifelse(f.breeding_pattern == "prolonged", 1,0),
            # utilizando valores absolutos para refletir distância da região equatorial
-           lat_max = abs(lat_max),
-           lat_min = abs(lat_min))
+           lat_max = lat_max,
+           abs.lat_min = abs(lat_min))
 
 # ESTATÍSTICA DESCRITIVA ------------------------------------------------------------------------------------------
 
-ranidae_completo %>% group_by(breeding_pattern) %>% 
-    summarise(n_species = n(),
-              min_area = min(total_area),
-              max_area = max(total_area),
-              mediana = median(total_area))
+ranidae_completo %>% filter(lat_min < 0) %>%
+    select(species, lat_max, lat_min)
+
+ranidae_completo %>% filter(lat_max < 0) %>%
+    select(species, lat_max, lat_min)
+
 
 ranidae_completo %>% group_by(breeding_pattern) %>% 
     summarise(n_species = n(),
-              min_lat = min(lat_min),
-              max_lat = max(lat_max),
-              mediana = mean(amplitude_lat))
+              min_area = min(area_occ_km2),
+              max_area = max(area_occ_km2),
+              mediana = median(area_occ_km2))
+
+# Latititude minima
+ranidae_completo %>% group_by(breeding_pattern) %>% 
+    summarise(n_species = n(),
+              min = min(lat_min),
+              max = max(lat_min),
+              mediana = median(lat_min))
+
+# Latititude minima
+ranidae_completo %>% group_by(breeding_pattern) %>% 
+    summarise(n_species = n(),
+              min = min(lat_max),
+              max = max(lat_max),
+              mediana = median(lat_max))
+
+# Centroide latitudinal
+ranidae_completo %>% group_by(breeding_pattern) %>% 
+    summarise(n_species = n(),
+              min = min(centroide_lat),
+              max = max(centroide_lat),
+              mediana = median(centroide_lat))
+
+# Amplitude latitudinal
+ranidae_completo %>% group_by(breeding_pattern) %>% 
+    summarise(n_species = n(),
+              min = min(amplitude_lat),
+              max = max(amplitude_lat),
+              mediana = median(amplitude_lat))
 
 # VERIFICANDO COLINEARIDADE -------------------------------------------------
 
@@ -76,34 +106,50 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
     if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
     text(0.5, 0.5, txt, cex = cex.cor * r)
 }
-pairs(ranidae_completo[,c(10:15,17)], upper.panel=panel.cor, diag.panel=panel.hist)
+pairs(ranidae_completo[,c(10:14,17)], upper.panel=panel.cor, diag.panel=panel.hist)
 
 
 
 # ANÁLISES ------------------------------------------------------------------------------------
 names(ranidae_completo)
 
-modelo1 <- glm(f.breeding_pattern ~ lat_min + lat_max + amplitude_lat, 
+modelo1 <- glm(f.breeding_pattern ~ lat_min + lat_max + amplitude_lat + lat_min*amplitude_lat + lat_max*amplitude_lat, 
                family = binomial, data = ranidae_completo)
 
-vif(modelo1)
+summary(modelo1)
+# nenhuma interação significativa
 
-modelo2 <- glm(f.breeding_pattern ~ lat_max + amplitude_lat, 
+modelo2 <- glm(f.breeding_pattern ~ lat_min + lat_max + amplitude_lat + lat_max*amplitude_lat, 
                family = binomial, data = ranidae_completo)
-
-vif(modelo2)
-
-simulateResiduals(modelo2, plot =T, n = 1000)
-# modelo validado
-
-drop1(modelo2, test = "Chisq")
 
 summary(modelo2)
+# interação não significativa
 
-modelo3 <- glm(f.breeding_pattern ~ lat_max, 
+
+modelo3 <- glm(f.breeding_pattern ~ lat_min + lat_max + amplitude_lat, 
                family = binomial, data = ranidae_completo)
 
-simulateResiduals(modelo3, plot =T, n = 1000)
+vif(modelo3)
+# algum erro 
+
+
+modelo4 <- glm(f.breeding_pattern ~ lat_max + amplitude_lat, 
+               family = binomial, data = ranidae_completo)
+
+vif(modelo4)
+
+
+simulateResiduals(modelo4, plot =T, n = 1000)
 # modelo validado
 
-summary(modelo3)
+drop1(modelo4, test = "Chisq")
+
+summary(modelo4)
+
+modelo5 <- glm(f.breeding_pattern ~ lat_max, 
+               family = binomial, data = ranidae_completo)
+
+simulateResiduals(modelo5, plot =T, n = 1000)
+# modelo validado
+
+summary(modelo5)
